a60eabb06cae5280f013198b98cedf882351d017
あまみや ゆうこ <1@1234.sh>
2016-07-30 23:47:59 +0800

    Fixed the solution

--- sample/LolitaConsoleAppSample/Program.cs
+++ sample/LolitaConsoleAppSample/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using LolitaConsoleAppSample.Models;
